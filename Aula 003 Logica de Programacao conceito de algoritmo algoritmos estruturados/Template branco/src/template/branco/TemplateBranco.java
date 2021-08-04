249ec3597efea90b981c07c4f5da38dd60ee6ebd
GABRIEL GIRARDI <gabriel.girardi@alunos.sc.senac.br>
2021-08-03 21:09:54 -0300

    upload logica Apache

--- Aula 003 Logica de Programacao conceito de algoritmo algoritmos estruturados/Template branco/src/template/branco/TemplateBranco.java
+++ Aula 003 Logica de Programacao conceito de algoritmo algoritmos estruturados/Template branco/src/template/branco/TemplateBranco.java
@@ -9,26 +9,43 @@ public class TemplateBranco {
 
     /**
      * @param args the command line arguments
-     */
+     */             int a = 3;
+            int b = 4;
+                        int c = 5;
+            int d = 6;
+                        int e = 7;
+            int f = a + b;
+                        int g = a + c;
+            int h = a + d;
+            
     public static void main(String[] args) {
-        String str2 = "Números inteiros somados";
-        String str1 = "Hello world";
-        int a = 1;
-        int b = 2;
-        int c = 3;
-        int d = 4;
-        int e = 5;
-        int f = a + b;
-        int g = a + c;
-        int h = a + d;
-     
-        System.out.println(str1);
-        System.out.println(str2);
-        System.out.println(" A + B - 1+2 = " + c); 
-        System.out.println(" A + C - 1+3 = " + d);
-        System.out.println(" A + D - 1+4 = " + e); 
-      
+       
         
+        
+        TemplateBranco Val = new TemplateBranco ();
+        String str2 = "Números inteiros somados ";
+        String str1 = "Hello world \n";
+            
+        
+            int [] ListNum = {1, 2, 3, 4, 6, 8, 10};
+            int resp = 11;
+             
+                System.out.print(str1);
+                System.out.println(str2);
+                System.out.print(str2 + str1);
+                System.out.printf("Soma da variveis a e b = %d \n", (Val.a + Val.b));
+                System.out.printf("Soma da variveis a e c = %d \n", (Val.a + Val.c));
+                System.out.printf("Soma da variveis a e d = %d \n", (Val.a + Val.d));
+                System.out.printf("Soma da variveis a e e = %d \n", (Val.a + Val.e));
+                    if(ListNum[0] + ListNum[6] == resp){
+                        System.out.print("resposta correta " + (ListNum[6] - ListNum[0]));
+                    }
+                    else{
+                        System.out.print ("resposta incorreta");
+                    }
+                            
+                                    
+                    
     }
     
 }
